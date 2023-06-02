c2e25aa4687fcdb62157820b25d187898e0e4054
markomilojevic2407993 <markomilojevic2407993@gmail.com>
2023-06-02 08:37:37 +0200

    2.6.

---
+++ forma.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <title></title>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link href="css/style.css" rel="stylesheet">
+    </head>
+    <body>
+
+
+
+            <form>
+                <input type="text" name="name" value="">
+                <input type="text" name="pass" value="">
+                <button type="submit">Pritisni</button>
+            </form>
+    
+    </body>
+</html>
--- logic.php
+++ logic.php
@@ -1,9 +1,23 @@
 <?php       
 
-$ime=$_POST['ime'];
 
 
-$pass=$_POST['pass'];
+
+if(isset($_POST['ime'])){
+    $ime=trim($_POST['ime']);            
+}
+
+
+//empty() funkcija koristi se isto kao i isset()
+//empty() funkcija je prayna !empty() funkcija netreba da bude prazna
+
+if(isset($_POST['pass'])){
+    $pass=trim($_POST['pass']);
+}
+
+if ($ime==""||$pass=="") {
+    die ("nisu uneti svi potrebni podaci"."<hr>");
+}
 
 
 echo $ime."<br>".$pass;
